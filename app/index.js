66fe8e22a1853470621f4b1bc027780022c67656
IlyaBielov <ibioriginalibi@gmail.com>
2019-06-15 17:21:42 +0300

    create handleBoxClick function

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
 
+var score = 0;
+
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
   $game.style.backgroundColor = '#fff';
@@ -10,7 +13,16 @@ function startGame() {
   renderBox();
 }
 
+function handleBoxClick(event) {
+  if (event.target.dataset) {
+    score++;
+    renderBox();
+  }
+}
+
 function renderBox() {
+  $game.innerHTML = '';
+  
   var box = document.createElement('div');
 
   box.style.height = box.style.width = '50px';
@@ -19,6 +31,7 @@ function renderBox() {
   box.style.top = '50px';
   box.style.left = '70px';
   box.style.cursor = 'pointer';
+  box.setAttribute('data-box', 'true');
 
   $game.insertAdjacentElement('afterbegin', box);
 }
