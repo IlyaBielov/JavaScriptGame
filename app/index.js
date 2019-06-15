1df5f9fc0b9715033fe6703762e9683163a9b437
IlyaBielov <ibioriginalibi@gmail.com>
2019-06-15 17:16:37 +0300

    create renderBox function

--- app/index.js
+++ app/index.js
@@ -6,4 +6,19 @@ $start.addEventListener('click', startGame);
 function startGame() {
   $game.style.backgroundColor = '#fff';
   $start.classList.add('hide');
+
+  renderBox();
+}
+
+function renderBox() {
+  var box = document.createElement('div');
+
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '70px';
+  box.style.cursor = 'pointer';
+
+  $game.insertAdjacentElement('afterbegin', box);
 }
