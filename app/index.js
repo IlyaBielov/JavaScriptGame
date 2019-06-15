d2a4bb9f85c92fe01b37260294ddb80a9f71a422
IlyaBielov <ibioriginalibi@gmail.com>
2019-06-15 17:07:19 +0300

    create startGame function

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+
+function startGame() {
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
+}
