0617987f09774a32b720b4ab23891a8fb64963b9
IlyaBielov <ibioriginalibi@gmail.com>
2019-06-15 17:44:30 +0300

    create timer and 'endGame' functions

--- app/index.js
+++ app/index.js
@@ -1,20 +1,43 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
+var $time = document.querySelector('#time');
 
 var score = 0;
+var isGameStarted = false;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
 function startGame() {
+  isGameStarted = true;
   $game.style.backgroundColor = '#fff';
   $start.classList.add('hide');
+
+  var interval = setInterval(function() {
+    var time = parseFloat($time.textContent);
+    
+    if (time <=0) {
+      clearInterval(interval);
+      endGame();
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+    
+  }, 100)
 
   renderBox();
 }
 
+function endGame() {
+  isGameStarted = false;
+}
+
 function handleBoxClick(event) {
-  if (event.target.dataset) {
+  if (!isGameStarted) {
+    return;
+  }
+  
+  if (event.target.dataset.box) {
     score++;
     renderBox();
   }
@@ -22,16 +45,23 @@ function handleBoxClick(event) {
 
 function renderBox() {
   $game.innerHTML = '';
-  
   var box = document.createElement('div');
-
-  box.style.height = box.style.width = '50px';
+  var boxSize = getRandom(30, 100);
+  var gameSize = $game.getBoundingClientRect();
+  var maxTop = gameSize.height - boxSize;
+  var maxLeft =  gameSize.width - boxSize;
+  
+  box.style.height = box.style.width = boxSize + 'px';
   box.style.position = 'absolute';
   box.style.backgroundColor = '#000';
-  box.style.top = '50px';
-  box.style.left = '70px';
+  box.style.top = getRandom(0, maxTop) + 'px';
+  box.style.left = getRandom(0, maxLeft) + 'px';
   box.style.cursor = 'pointer';
   box.setAttribute('data-box', 'true');
 
   $game.insertAdjacentElement('afterbegin', box);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max-min) + min);
 }
